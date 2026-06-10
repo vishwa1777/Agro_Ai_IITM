44706e31d086f147e55b2b08d984b0911bba33f6
Anmol Kumar <anmolkumar875511@gmail.com>
2026-06-10 11:36:01 +0530

    rename fixed

---
+++ backend/src/controllers/analytics.controller.js
@@ -0,0 +1,32 @@
+import {
+  revenueAnalytics,
+  visitAnalytics,
+  cropRiskAnalytics
+} from "../services/analytics.service.js";
+
+export const getRevenue =
+  async (req, res) => {
+
+    const data =
+      await revenueAnalytics();
+
+    res.json(data);
+  };
+
+export const getVisits =
+  async (req, res) => {
+
+    const data =
+      await visitAnalytics();
+
+    res.json(data);
+  };
+
+export const getCropRisk =
+  async (req, res) => {
+
+    const data =
+      await cropRiskAnalytics();
+
+    res.json(data);
+  };
