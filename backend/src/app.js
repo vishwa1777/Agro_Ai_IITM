5812acf4353216d6eb3617d483df75bd501ea88b
Anmol Kumar <anmolkumar875511@gmail.com>
2026-06-10 11:33:41 +0530

    all routes added to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,15 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 import errorHandler from "./middleware/errorHandler.js";
+import authRoutes from "./routes/auth.routes.js";
+import dashboardRoutes from "./routes/dashboard.routes.js";
+import retailerRoutes from "./routes/retailer.routes.js";
+import growerRoutes from "./routes/grower.routes.js";
+import productRoutes from "./routes/product.routes.js";
+import visitRoutes from "./routes/visit.routes.js";
+import taskRoutes from "./routes/task.routes.js";
+import analyticsRoutes from "./routes/analytics.routes.js";
+import recommendationRoutes from "./routes/recommendation.routes.js";
 
 const app = express();
 
@@ -21,6 +30,16 @@ app.get("/", (req, res) => {
     message: "AgroAI API Running"
   });
 });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/retailers", retailerRoutes);
+app.use("/api/growers", growerRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/visits", visitRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/analytics", analyticsRoutes);
+app.use("/api/recommendations", recommendationRoutes);
 
 app.use(errorHandler);
 
